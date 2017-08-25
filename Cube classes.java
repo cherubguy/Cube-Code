8df698827eceefb901be8101b7ebd52fb1ad8dd7
cherubguy <cherubguy@gmail.com>
2017-08-25 15:18:53 +1200

    Add files via upload

---
+++ Cube classes.java
@@ -0,0 +1,26 @@
+class MainClass{
+	public static void main(String[] args){
+		class Cube{
+			public int sides = 6;
+			public int vertices = 8;
+			private int sideLength;
+			Cube(int x){
+				sideLength = x;
+			}
+			public static elongate(int moreLength){
+				this.sideLength =+ moreLength
+			}
+			public static widen(int moreWidth){
+				this.
+		}		
+		class Rectangle extends Cube{
+			private int height;
+			private int width;
+			private int depth;
+			Rectangle(int x, y, z){
+				height = x;
+			    width = y;
+				depth = z;
+			}
+		}	
+	    	
---
+++ Cube implementation.java
@@ -0,0 +1,4 @@
+class MainClass{
+	public static void main(String[] args){
+		Cube myCube(4) = new Cube;
+		Rectangle myRectangle(4, 5, 6) = new Rectangle;
