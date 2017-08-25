2929565bd393b94135b8d73157b473728538cb9f
cherubguy <cherubguy@gmail.com>
2017-08-25 16:03:10 +1200

    Update Cube classes.java

--- Cube classes.java
+++ Cube classes.java
@@ -1,17 +1,39 @@
 class MainClass{
 	public static void main(String[] args){
-		class Cube{
+		class Cube extends Shape{
+			public String status = 'Cube'
 			public int sides = 6;
 			public int vertices = 8;
-			private int sideLength;
+			private int cubeLength;
+			private int cubeHeight;
+			private int cubeWidth;
 			Cube(int x){
-				sideLength = x;
+				cubeLength = x;
+				cubeHeight = x;
+				cubeWidth = x;
 			}
-			public static elongate(int moreLength){
-				this.sideLength =+ moreLength
+			public static boolean lengthen(int moreLength){
+				this.cubeLength =+ moreLength;
+				if (moreLength > 0){
+				    return True;
+				}else{
+					return False;
 			}
-			public static widen(int moreWidth){
-				this.
+			public static boolean heighten(int moreHeight){
+				this.cubeHeight =+ moreHeight;
+				if (moreHeight > 0){
+				    return True;
+				}else{
+					return False;
+			}
+			public static boolean widen(int moreWidth){
+				this.cubeWidth =+ moreWidth;
+				if (moreWidth > 0){
+				    return True;
+				}else{
+					return False;
+			}
+			if (widen == True || lengthen == True || heighten == True){
 		}		
 		class Rectangle extends Cube{
 			private int height;
@@ -22,5 +44,5 @@ class MainClass{
 			    width = y;
 				depth = z;
 			}
+		
 		}	
-	    	
